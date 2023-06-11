3a8cbc6a399d624833fd9f29445e35fc490e14ba
CGomezGomez <cggomez93@gmail.com>
2023-06-11 16:06:49 +0200

    Ultimos Cambios

--- src/components/Overview/styles.js
+++ src/components/Overview/styles.js
@@ -1,27 +1,31 @@
 import styled from "styled-components";
 
 const StyledSocial = styled.div`
+
     width: 255px;
     display: flex;
     flex-direction: column;
-    cursor: pointer;
     background-color: ${props => (props.active 
         ? 'rgba(37, 43, 66, 1)' 
         : 'rgba(241, 243, 250, 1)')};
     border-radius: 6px;
+    cursor: pointer;
   
 `;
 
 const StyledRow = styled.div`
+
     width: 80%;
     margin: auto;
     padding-top: 15px;
     display: flex;
     justify-content: space-between;
     align-items: center;
+
 `;
 
 const StyledH2 = styled.h2`
+
     font-family: 'Inter';
     font-style: normal;
     font-weight: 700;
@@ -30,9 +34,11 @@ const StyledH2 = styled.h2`
     color: ${props => (props.active 
         ? 'rgba(140, 152, 198, 1)' 
         : 'rgba(140, 152, 198, 1)')};
+
 `;
 
 const StyledH3 = styled.h3`
+
     font-family: 'Inter';
     font-style: normal;
     font-weight: 700;
@@ -41,9 +47,11 @@ const StyledH3 = styled.h3`
     color: ${props => (props.active 
         ? 'white' 
         : 'rgba(29, 31, 41, 1)')};
+       
 `;
 
 const StyledP = styled.p`
+
     font-family: 'Inter';
     font-style: normal;
     font-weight: 700;
